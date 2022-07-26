caac6af9817b7055aeeef4d8711dea7863ef8f16
Fares Emanuel Gómez Cortés <fe.gomez.corter@gmail.com>
2022-07-25 22:22:04 -0500

    chore: First commit, ZombieArena project
    Signed-off-by: Fares Emanuel Gómez Cortés <fe.gomez.corter@gmail.com>

---
+++ ZombieArena.h
@@ -0,0 +1,13 @@
+//
+// Created by fegom on 24/07/2022.
+//
+
+#include <SFML/Graphics/VertexArray.hpp>
+
+#ifndef ZOMBIEARENA_ZOMBIEARENA_H
+#define ZOMBIEARENA_ZOMBIEARENA_H
+
+#endif //ZOMBIEARENA_ZOMBIEARENA_H
+#pragma once
+using namespace sf;
+int createBackground(VertexArray& rVA, IntRect arena);
